a1d8d3ef9fafd66d6a49653c19d108ca2c5906be
Nguyễn Duy Bách <nguyenduybach12a11@gmail.com>
2024-11-04 21:11:20 +0700

    rename filmDetail

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,7 +1,7 @@
 import { lazy } from 'react';
 
 export const HomePage = lazy(() => import('./Home/HomePage'));
-export const FilmDetailPage = lazy(() => import('./FlimDetail/FlimDetail'));
+export const FilmDetailPage = lazy(() => import('./FilmDetail/FilmDetail'));
 export const MediaPage = lazy(() => import('./Media/MediaPage'));
 export const LoginPage = lazy(() => import('./Authenticate/Login/Login'));
 export const SignUpPage = lazy(() => import('./Authenticate/SignUp/SignUp'));
